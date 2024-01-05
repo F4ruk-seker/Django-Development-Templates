33ac724f64fce70a0b9ec04b6b67c99e33c94d29
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-01-05 14:25:37 +0300

    set asgi,wsgi file DJANGO_SETTINGS_MODULE

--- config/asgi.py
+++ config/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PasswordApp.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.develop')
 
 application = get_asgi_application()
