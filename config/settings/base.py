3cefc24d9191b54bbb5a572e4e03ff140f3461ac
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-01-06 14:01:09 +0300

    add rest DEFAULT_AUTHENTICATION_CLASSES

--- config/settings/base.py
+++ config/settings/base.py
@@ -66,6 +66,13 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=15),
--- config/settings/product.py
+++ config/settings/product.py
@@ -50,3 +50,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
+
