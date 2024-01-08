449490f6ad358082a15817f39801fe35d3e83f06
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-01-08 12:11:07 +0300

    fix config.url

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
-from settings.base import env
+from config.settings.base import env
 
 
 urlpatterns = [
