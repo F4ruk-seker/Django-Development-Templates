ec1b05bf089bb07f03dafdef935a66aa6bc67ffe
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-01-08 10:40:08 +0300

    set admin url path

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from config.settings.base import env
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/' if settings.DEBUG else env('PRODUCT_ADMIN_PATH'), admin.site.urls),
 ]
