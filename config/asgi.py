15cc6bc8f9138459328f9c10eaffbcda862db93e
F4ruk.seker <ahmetfaruk731@gmail.com>
2024-06-17 19:23:53 +0300

    Change app name

--- config/asgi.py
+++ config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for PasswordApp project.
+ASGI config for MyDjangoApp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for PasswordApp project.
+WSGI config for MyDjangoApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
